073c578b3f7bb4b935371acc9f43c4d06ab77187
C-BESSY <charistyfirst@gmail.com>
2024-08-13 17:24:49 +0700

    feat: update

--- lua/community.lua
+++ lua/community.lua
@@ -5,6 +5,39 @@
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
+  -- UI
+  { import = "astrocommunity.colorscheme.monokai-pro-nvim" },
+  { import = "astrocommunity.color.tint-nvim" },
+  { import = "astrocommunity.bars-and-lines.dropbar-nvim" },
+  { import = "astrocommunity.bars-and-lines.lualine-nvim" },
+  { import = "astrocommunity.recipes.telescope-lsp-mappings" },
+
+  -- LSP
+  { import = "astrocommunity.lsp.lsp-signature-nvim" },
+  { import = "astrocommunity.lsp.actions-preview-nvim" },
+
+  -- Git
+  { import = "astrocommunity.git.blame-nvim" },
+
+  -- Plugins
+  { import = "astrocommunity.utility.noice-nvim" },
+  { import = "astrocommunity.indent.mini-indentscope" },
+  { import = "astrocommunity.diagnostics.trouble-nvim" },
+  { import = "astrocommunity.terminal-integration.vim-tmux-navigator" },
+  { import = "astrocommunity.file-explorer.telescope-file-browser-nvim" },
+
+  -- Motions
+  { import = "astrocommunity.motion.flit-nvim" },
+  { import = "astrocommunity.motion.mini-move" },
+  { import = "astrocommunity.motion.nvim-surround" },
+  { import = "astrocommunity.motion.leap-nvim" },
+  { import = "astrocommunity.motion.marks-nvim" },
+
+  -- Path
   { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.pack.dart" },
+  { import = "astrocommunity.pack.blade" },
+  { import = "astrocommunity.pack.tailwindcss" },
 }
